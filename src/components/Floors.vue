e9b7fdca47f8eb7091300c3bd7a42166d6af71a6
Mirek Kdzierski <mirekkedzierski@outlook.com>
2020-03-15 10:13:52 +0100

    Start project

--- src/components/Floors.vue
+++ src/components/Floors.vue
@@ -1,20 +1,44 @@
 <template>
   <div clas="container">
      <h2>Floors</h2>
+     {{ filteRoomList }}<br />
+     <button @click="createRoomList">Create List</button>
+     <hr>
+     <Rooms :listOfRooms="filteRoomList" v-on:rebuild-list="rebuildRoomLisr" />
   </div>
 </template>
 
 <script>
 // Import components
-
+import Rooms from "./Rooms.vue"
 export default {
    name:'Floors',
+   components:{
+      Rooms
+   },
    data(){
       return{
-         
+         rooms:[],
+         index:34
       }
    },
+   methods:{
+      createRoomList(){
+         this.rooms=[];
+         this.index++
+         for(let i=0; i<this.index; i++){
+            this.rooms.push(i);
+         }
+      },
+      rebuildRoomLisr(val){
+         this.rooms = this.rooms.filter(x => x !=val);
+      }
+   },
+   
    computed:{
+      filteRoomList(){
+         return this.rooms;
+      }
    }
    
 }
---
+++ src/components/Rooms.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <div class="flex">
+      <div :key='val' v-for="val in listOfRooms" >
+        <div class="el"> {{ val }}</div>
+        <div class="close" @click="close(val)">x</div>
+      </div>
+      {{listOfRooms}}
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Rooms',
+    props:['listOfRooms'],
+    data(){
+        return {
+            show:true
+        }
+    },
+    methods:{
+        close(val){
+            this.$emit('rebuild-list', val)
+        }
+    }
+}
+</script>
+
+<style>
+.flex{
+    display: flex;
+    flex-wrap: wrap;
+}
+.el{
+    width: 20px;
+    height: 20px;
+    border:1px solid palevioletred;
+    margin:4px;
+}
+.close{
+    height: 10px;
+    width:10px;
+    background: rebeccapurple;
+    text-indent: 1000px;
+    overflow: hidden;
+}
+</style>
